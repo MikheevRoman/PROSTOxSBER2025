fa962fb7683637b7cdb77a0462b83dee403bd50d
Dmitry Osobov <waffencode@gmail.com>
2025-03-30 04:59:41 +0300

    Add Telegram init function

--- kate-frontend/src/index.tsx
+++ kate-frontend/src/index.tsx
@@ -2,6 +2,9 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
+import { init } from '@telegram-apps/sdk';
+
+init();
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
